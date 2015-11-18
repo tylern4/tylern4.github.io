c863f50c10a766e18dd273651ae45d036c306934
Nick Tyler <tylern@canisius.edu>
2015-11-18 12:11:14 -0500

    add booked webgl plot

--- bokeh_test.py
+++ bokeh_test.py
@@ -1,26 +1,16 @@
-from bokeh.charts import Histogram
-from bokeh.sampledata.autompg import autompg as df
-from bokeh.charts import defaults, vplot, hplot, show, output_file
+import numpy as np
 
-defaults.width = 450
-defaults.height = 350
+from bokeh.plotting import figure, show, output_file
 
-# input options
-hist = Histogram(df['mpg'], title="df['mpg']")
-hist2 = Histogram(df, 'displ', title="df, 'displ'")
-hist3 = Histogram(df, values='hp', title="df, values='hp'")
+N = 10000
 
-hist4 = Histogram(df, values='hp', color='cyl',
-                  title="df, values='hp', color='cyl'", legend='top_right')
+x = np.random.normal(0, np.pi, N)
+y = np.sin(x) + np.random.normal(0, 0.2, N)
 
-hist5 = Histogram(df, values='mpg', bins=50,
-                  title="df, values='mpg', bins=50")
+output_file("scatter10k.html", title="scatter 10k points (with WebGL)")
 
-output_file("histograms.html")
+p = figure(webgl=True)
+p.scatter(x,y, alpha=0.1)
 
-show(
-    vplot(
-        hplot(hist, hist2, hist3),
-        hplot(hist4, hist5)
-    )
-)
+output_file("webgl.html")
+show(p)
